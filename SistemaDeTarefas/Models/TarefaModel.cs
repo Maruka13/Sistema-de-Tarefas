bbe845d32854b609bb661e8da5c37be65247c896
Maruka <emanuelle_carollina@hotmail.com>
2024-11-07 19:32:35 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Models/TarefaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descrição { get; set; }
+        public int Status { get; set; } 
+
+    }
+}
